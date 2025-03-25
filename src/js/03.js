a6e269e61d5a3692071e713bb8cf1e6beb4ec666
Pasha0923 <alexsai@i.ua>
2025-03-25 19:03:10 +0300

    add async/await

--- src/js/03.js
+++ src/js/03.js
@@ -1,64 +1,39 @@
 /**
  * Використовуємо https://pokeapi.co/ та створимо сторінку перегляду покемонів
+ *
+ * Переписуємо на async/await
  */
-
-function fetchPokemon(pokemonId) {
-  return fetch(
-    `https://pokeapi.co/api/v2/pokemon/${pokemonId.toLowerCase()}`
-  ).then(resp => {
-    if (!resp.ok) {
-      throw new Error(resp.statusText);
-    }
-
-    return resp.json();
-  });
-}
+import { fetchPokemon } from './services/pockemonApi.js'; // named import
+import renderPokemonCard from './templates/pockemonCard.js'; // default import
 
 const cardContainer = document.querySelector('.card-container');
 const searchForm = document.querySelector('.search-form');
 
-searchForm.addEventListener('submit', handleSearch);
+searchForm.addEventListener('submit', onSearch);
 
-function handleSearch(event) {
-  event.preventDefault();
+async function onSearch(e) {
+  e.preventDefault();
 
-  const form = event.currentTarget;
-  const query = form.elements.query.value;
+  const form = e.currentTarget;
+  const searchQuery = form.elements.query.value;
 
-  console.log(query);
+  try {
+    const data = await fetchPokemon(searchQuery);
+    renderPokemonCard(data, cardContainer);
+  } catch (err) {
+    console.log(err);
+    onFetchError();
+  } finally {
+    form.reset();
+  }
 
-  fetchPokemon(query)
-    .then(renderPokemonCard)
-    .catch(onFetchError)
-    .finally(() => form.reset());
-
-  //.then(data => renderPokemonCard(data))
+  // fetchPokemon(searchQuery)
+  //   .then(renderPokemonCard)
+  //   .catch(onFetchError)
+  //   .finally(form.reset);
 }
 
-function renderPokemonCard({ name, sprites, weight, height, abilities }) {
-  const abilityListItems = abilities
-    .map(ability => `<li class="list-group-item">${ability.ability.name}</li>`)
-    .join('');
-  console.log(abilityListItems);
-
-  const markup = `<div class="card">
-
-   <div class="card-img-top">
-     <img src="${sprites.front_default}" alt="${name}">
-   </div>
-   <div class="card-body">
-     <h2 class="card-title">Ім'я: ${name}</h2>
-     <p class="card-text">Вага: ${weight} g</p>
-     <p class="card-text">Зростання: ${height} ft</p>
-
-     <p class="card-text"><b>Уміння</b></p>
-     <ul class="list-group">${abilityListItems}</ul>
-   </div>
-</div>`;
-  cardContainer.innerHTML = markup;
-}
-
-function onFetchError(error) {
+function onFetchError() {
+  cardContainer.innerHTML = '';
   alert('Упс, щось пішло не так і ми не знайшли вашого покемона!');
-  console.error(error);
 }
